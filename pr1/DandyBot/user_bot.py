8afdf07b261f4f16057e3c9ee353840396fc8753
Paj_abuser <philippstriz@mail.ru>
2023-02-14 01:33:16 +0300

    zxc

---
+++ pr1/DandyBot/user_bot.py
@@ -0,0 +1,8 @@
+﻿def script(check, x, y):
+    if check("gold", x, y):
+            return "take"
+    if ~check("wall", x+1, y):
+            return "right"
+    elif ~check("wall", x, y+1):
+        return "down"
+    return "pass"
