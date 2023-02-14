e43dd0696f4fb8330ff0bba132386548c24c0aea
Paj_abuser <philippstriz@mail.ru>
2023-02-14 21:34:37 +0300

    zxc

--- pr1/main.py
+++ pr1/main.py
@@ -1,5 +1,4 @@
 import math
-import random
 import tkinter as tk
 
 
@@ -12,12 +11,35 @@ def pyshader(func, w, h):
                             for c in func(x / w, y / h)]
     return bytes('P6\n%d %d\n255\n' % (w, h), 'ascii') + scr
 
+def fact(n):
+    if n == 0:
+        return 1
+    else:
+        return n * fact(n - 1)
+
+def pseudoRandom(x, y):
+    return x*math.sin(x  % fact(int(y * 2))) + y*math.cos(y  % fact(int(x * 2)))
+
+def noise(x, y):
+    return (math.sin(pseudoRandom(x, y) * 1000) + math.cos(pseudoRandom(x, y) * 1000))
+
+def lerp(a, b, x):
+    return a + x * (b - a)
+
+def val_noise(x, y):
+    n = 255
+    n += 1
+    x2 = round(n*x) % n
+    y2 = round(n*y) % n
+    return noise(x2, y2)
+    
+
 def func(x, y):
-    r1 = 80/255
-    r2 = 20/255
-    circ1 = (x - 0.48)**2 + (y - 0.48)**2
-    circ2 = (x - 0.52)**2 + (y - 0.52)**2
-    return 1-(circ2)/(r1**2), 1-(circ1)/(r1**2), 0
+    x += 0
+    y += 0
+    val = noise(x, y)
+    val = val_noise(x, y)
+    return val, val, val
 
 
 label = tk.Label()
