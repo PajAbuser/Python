8f12b5c94c94964be8560ba0954fcdcb06bfc573
Paj_abuser <philippstriz@mail.ru>
2023-02-15 23:18:05 +0300

    xcz

--- pr1/main.py
+++ pr1/main.py
@@ -11,33 +11,29 @@ def pyshader(func, w, h):
                             for c in func(x / w, y / h)]
     return bytes('P6\n%d %d\n255\n' % (w, h), 'ascii') + scr
 
-def fact(n):
-    if n == 0:
-        return 1
-    else:
-        return n * fact(n - 1)
-
 def pseudoRandom(x, y):
-    return x*math.sin(x  % fact(int(y * 2))) + y*math.cos(y  % fact(int(x * 2)))
+    return x*math.sin(( y * 255)) + y*math.cos((x * 255) )
 
 def noise(x, y):
-    return (math.sin(pseudoRandom(x, y) * 1000) + math.cos(pseudoRandom(x, y) * 1000))
+    return (math.sin(pseudoRandom(x, y) * 1000) + math.cos(pseudoRandom(y, x) * 1000))
 
 def lerp(a, b, x):
     return a + x * (b - a)
 
 def val_noise(x, y):
-    n = 255
-    n += 1
-    x2 = round(n*x) % n
-    y2 = round(n*y) % n
-    return noise(x2, y2)
+    n = 800
+    #n = 127*(x+y) + 1
+    x2  = ( ( (n*x) + 1 ) / ( round(n*x) + 1) ) % n
+    y2  = ( ( (n*y) + 1 ) / ( round(n*y) + 1) ) % n
+    x22 = ( ( (n*x) + 2 ) / ( round(n*x) + 2) ) % n
+    y22 = ( ( (n*y) + 2 ) / ( round(n*y) + 2) ) % n
+    return noise(x2,y2)*(x2+y2) + (1 - noise(x22,y22)*(x22+y22))
     
 
 def func(x, y):
     x += 0
     y += 0
-    val = noise(x, y)
+    #val = noise(x, y)
     val = val_noise(x, y)
     return val, val, val
 
